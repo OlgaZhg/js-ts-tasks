57d8ac214fd65b344f904582d70a56edf9a86038
Olga Zhgirovskaya <zhgirovskaya.olga@student.ehu.lt>
2024-11-06 17:21:14 +0100

    feat: solved task 4

--- src/digitsSum.js
+++ src/digitsSum.js
@@ -3,6 +3,19 @@
  * @param {number} n - number
  * @returns {number}
  */
+
 module.exports.digitsSum = function digitsSum(n) {
-  throw new Error('Not implemented'); // remove this line
+  let result = 0;
+  const nAbs = Math.abs(n);
+  const nString = nAbs.toString();
+  if (nString[0] !== "-") {
+    result += nString[0] * 1;
+  }
+
+  for (let i = 1; i < nString.length; ++i) {
+
+    result += nString[i] * 1;
+  }
+
+  return result;
 };
