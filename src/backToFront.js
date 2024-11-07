6aa96c76d63b4cb999286175b49e4aa43fde1667
Olga Zhgirovskaya <zhgirovskaya.olga@student.ehu.lt>
2024-11-07 23:22:02 +0100

    feat: solved task 5

--- src/backToFront.js
+++ src/backToFront.js
@@ -1,9 +1,18 @@
 /**
- * Copy and paste defined amount of a string's symbols to the front and back of a string
+ * Copy and addPart defined amount of a string's symbols to the front and back of a string
  * @param {string} str
  * @param {number} symbolsCount
  * @returns {string}
  */
 module.exports.backToFront = function backToFront(str, symbolsCount) {
-  throw new Error('Not implemented');
+  let addPart = "";
+
+  if (symbolsCount > str.length) {
+    return str;
+  }
+  for (let i = str.length - 1; i > (str.length - 1) - symbolsCount; i--) {
+    addPart = `${str[i]}${addPart}`;
+  }
+
+  return `${addPart}${str}${addPart}`;
 };
