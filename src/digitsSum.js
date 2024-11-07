af3ebf22da3303b93e96bca32a9b4f4d6507ffdd
Olga Zhgirovskaya <zhgirovskaya.olga@student.ehu.lt>
2024-11-08 00:05:16 +0100

    feat: solved task 4

--- src/digitsSum.js
+++ src/digitsSum.js
@@ -8,10 +8,10 @@ module.exports.digitsSum = function digitsSum(n) {
   let result = 0;
   const nAbs = Math.abs(n);
   const nString = nAbs.toString();
+
   if (nString[0] !== "-") {
     result += nString[0] * 1;
   }
-
   for (let i = 1; i < nString.length; ++i) {
     result += nString[i] * 1;
   }
