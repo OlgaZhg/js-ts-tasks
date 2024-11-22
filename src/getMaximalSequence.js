30c000442bd8044a9df2d2330c86184256a87a20
Olga Zhgirovskaya <zhgirovskaya.olga@student.ehu.lt>
2024-11-22 23:59:17 +0100

    feat: solved task 2

--- src/getMaximalSequence.js
+++ src/getMaximalSequence.js
@@ -4,21 +4,22 @@
  * @returns {Object}
  */
 module.exports.getMaximalSequence = function getMaximalSequence(arr) {
-  let countCurrent = 0;
-  let countMax = 1;
-  let indexCurrent = 0;
-  let indexMax = 0;
+  let countCurrent = 0; // length of the sequence of equal elements that is being checked
+  let countMax = 0; // length of the maximal sequence of equal elements
+  let indexCurrent = 0; // index of the first element in countCurrent
+  let indexMax = 0; // index of the first element in countMax
 
   if(arr.length === 0) {
     return arr;
   }
-  countMax = 1;
+  countCurrent = 1; // arr contains at least 1 element
+  countMax = 1; // arr contains at least 1 element
 
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] === arr[indexCurrent]) {
       countCurrent++;
     }
-    if((arr[i] !== arr[indexCurrent]) || (i===arr.length - 1)) {
+    if((arr[i] !== arr[indexCurrent]) || (i===arr.length - 1)) { // 2 conditions in 1 'if' because the same evaluations are needed for both
       if(countCurrent > countMax) {
         countMax = countCurrent;
         indexMax = indexCurrent;
@@ -26,7 +27,6 @@ module.exports.getMaximalSequence = function getMaximalSequence(arr) {
       indexCurrent = i;
       countCurrent = 1;
     }
-
   }
   return arr.slice(indexMax, indexMax + countMax);
 }
