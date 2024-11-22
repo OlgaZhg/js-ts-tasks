9bf0139a83c3ab03ee73a582ac372b48897a5cc6
Olga Zhgirovskaya <zhgirovskaya.olga@student.ehu.lt>
2024-11-22 16:33:28 +0100

    feat: draft for task 4

--- src/removeItems.js
+++ src/removeItems.js
@@ -5,5 +5,35 @@
  * @returns {Object}
  */
 module.exports.removeItems = function removeItems(arr, value) {
-  throw new Error('Not implemented'); // remove me and write a solution
-};
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== typeof value) {
+      continue;
+    }
+    if (typeof value === "object") {
+      let arrKeys = Object.keys(arr[i]);
+      let valueKeys = Object.keys(value);
+
+      if (arrKeys.length !== valueKeys.length) {
+        continue;
+      } else {
+        let objEqual = true;
+        for (let key of arrKeys) {
+          if (valueKeys.includes(key)) {
+            if (arr[i][key] !== value[key]) {
+              objEqual = false;
+              break;
+            }
+          }
+        }
+        if (objEqual) {
+          arr.splice(i, 1);
+          i--;
+        }
+      }
+    } else if (arr[i] === value) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
+}
