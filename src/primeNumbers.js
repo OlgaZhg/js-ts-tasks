cf42271cec6e1bab5876a08595afbc8e9acc23b2
Olga Zhgirovskaya <zhgirovskaya.olga@student.ehu.lt>
2024-11-29 16:48:06 +0100

    feat: solved task 4

--- src/primeNumbers.js
+++ src/primeNumbers.js
@@ -4,5 +4,25 @@
  * @returns {function}
  */
 module.exports.primeNumbers = function primeNumbers(highestNumber) {
-  throw new Error('Not implemented'); // remove me and write a solution
-};
+
+    return function arrayPrimeNumbers(fnArgument1, fnArgument2) {
+      if (fnArgument2 > highestNumber) {
+        fnArgument2 = highestNumber;
+      }
+      const arrPrimeNumbers = [];
+      let isPrime = true;
+      for (let numToCheck = fnArgument1; numToCheck <= fnArgument2; numToCheck++) {
+        isPrime = true;
+        for (let i = 2; i <= Math.sqrt(numToCheck); i++) {
+          if (numToCheck % i === 0) {
+            isPrime = false;
+            break;
+          }
+        }
+        if (isPrime) {
+          arrPrimeNumbers.push(numToCheck);
+        }
+      }
+      return arrPrimeNumbers;
+    }
+  };
